2823539a4fbf1f12eb4eb66f4f71019d92b928ae
Jefflima11 <jeffersonl.1105@gmail.com>
2022-01-25 00:33:52 -0300

    dev news procedures

--- Script initial data base gri.sql
+++ Script initial data base gri.sql
@@ -39,3 +39,38 @@ constraint hist_marca_fk foreign key (cd_marca) references marca (cd_marca),
 constraint hist_tamanho_fk foreign key (cd_tamanho) references tamanho (cd_tamanho),
 constraint hist_tp_peca_fk foreign key (cd_tp_peca) references tp_peca (cd_tp_peca)
 );
+
+
+create table pro_inat (
+cd_pro_inat int AUTO_INCREMENT PRIMARY key,
+cd_hist_pro int not null,
+dt_inativacao date not NULL,
+constraint inat_pro_pk FOREIGN KEY (cd_hist_pro) REFERENCES hist_pro (cd_hist_pro)
+);
+
+ DELIMITER $$
+ create PROCEDURE sp_hist_pro (
+ pcd_produto int,
+ pcd_marca int,
+ pcd_tamanho int,
+ pcd_tp_peca int,
+ pds_observacao varchar(255)    
+ )
+ BEGIN
+
+     start transaction;
+
+     if not exists(select cd_produto
+                   from hist_pro
+                   where cd_produto = pcd_produto
+                     and cd_marca = pcd_marca
+                     and cd_tamanho = pcd_tamanho
+                     and cd_tipo_peca = pcd_tipo_peca
+                     and ds_observacao = pds_observacao
+                     and cd_hist_pro not in (select cd_hist_pro))
+
+
+ end $$
+
+
+ DELIMITER ;
